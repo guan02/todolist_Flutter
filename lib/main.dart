1e1fa8c31b937085d125949ca78f20073f9f5354
francesco_guan <guan.francesco2002@gmail.com>
2023-05-20 17:35:45 +0000

    separazione del home principale

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
+import 'MyHomePage.dart';
 
-void main() => runApp(MyApp(
-      items: List<ListItem>.generate(
-        10,
-        (i) => MessageItem('Sender $i', 'Message body $i'),
-      ),
-    ));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.items});
+  const MyApp({super.key});
   static const String _title = 'todoList_Flutter';
-  final List<ListItem> items;
+  //final List<ListItem> items;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,99 +18,4 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  final List<ListItem> items = [];
-  TextEditingController testcontroller = TextEditingController();
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Flutter Demo Click Counter'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(8),
-        children: [
-          for (var i in items)
-            Container(
-              alignment: Alignment.center,
-              color: Colors.grey,
-              width: double.infinity,
-              height: 100,
-              margin: EdgeInsets.only(bottom: 8),
-              child: (ListTile(
-                title: i.buildTitle(context),
-                subtitle: i.buildSubtitle(context),
-              )),
-            )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-/// The base class for the different types of items the list can contain.
-abstract class ListItem {
-  /// The title line to show in a list item.
-  Widget buildTitle(BuildContext context);
-
-  /// The subtitle line, if any, to show in a list item.
-  Widget buildSubtitle(BuildContext context);
-}
-
-class HeadingItem implements ListItem {
-  final String heading;
-
-  HeadingItem(this.heading);
-
-  @override
-  Widget buildTitle(BuildContext context) {
-    return Text(
-      heading,
-      style: Theme.of(context).textTheme.headlineSmall,
-    );
-  }
-
-  @override
-  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
-}
-
-/// A ListItem that contains data to display a message.
-class MessageItem implements ListItem {
-  final String sender;
-  final String body;
-
-  MessageItem(this.sender, this.body);
-
-  @override
-  Widget buildTitle(BuildContext context) => Text(sender);
-
-  @override
-  Widget buildSubtitle(BuildContext context) => Text(body);
 }
